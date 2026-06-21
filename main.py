2c126b855491f1238d5f4f79242043ed9d0a5d75
Adhit <adhitkulkarni@gmail.com>
2026-06-21 18:50:23 +0530

    fix postgres autoincrement issue

--- main.py
+++ main.py
@@ -32,25 +32,31 @@ def init_db():
     conn = get_conn()
     cur = conn.cursor()
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE,
-        password TEXT
-    )""")
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            username TEXT UNIQUE,
+            password TEXT
+        )
+    """)
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS friends(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        sender TEXT,
-        receiver TEXT,
-        status TEXT
-    )""")
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS friends(
+            id SERIAL PRIMARY KEY,
+            sender TEXT,
+            receiver TEXT,
+            status TEXT
+        )
+    """)
 
-    cur.execute("""CREATE TABLE IF NOT EXISTS messages(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        sender TEXT,
-        receiver TEXT,
-        message TEXT
-    )""")
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS messages(
+            id SERIAL PRIMARY KEY,
+            sender TEXT,
+            receiver TEXT,
+            message TEXT
+        )
+    """)
 
     conn.commit()
     conn.close()
